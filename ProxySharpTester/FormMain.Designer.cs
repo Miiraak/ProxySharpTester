a3cc4d38da7cf61403098a6a87f0e28ec97582d9
Miiraak <miiraak@feynlok.ch>
2024-09-07 14:14:06 +0200

    Enhance proxy filtering and update ProxySharp package
    - Removed `buttonRenewFilteredProxies` and associated controls.
    - Added new buttons and controls for filtering by country, port, HTTPS support, and anonymity level.
    - Adjusted layout and updated `ClientSize` for new controls.
    - Removed `ButtonRenewFilteredProxies_Click` method.
    - Added event handlers for new filter buttons.
    - Updated `ProxySharpTester.csproj` to use ProxySharp v2.0.2.9.

--- ProxySharpTester/FormMain.Designer.cs
+++ ProxySharpTester/FormMain.Designer.cs
@@ -36,19 +36,25 @@
             buttonGetIndex = new Button();
             buttonPopProxy = new Button();
             buttonAddProxy = new Button();
-            buttonRenewFilteredProxies = new Button();
+            buttonFilterCountry = new Button();
             richTextBoxProxies = new RichTextBox();
             buttonClearLog = new Button();
-            textBoxValue1 = new TextBox();
+            textBoxCountry = new TextBox();
             label3 = new Label();
             textBoxAddProxyIP = new TextBox();
             textBoxGetIndex = new TextBox();
             label4 = new Label();
-            checkBoxRenewFilteredProxiesExclude1 = new CheckBox();
+            checkBoxCountry = new CheckBox();
             textBoxAddProxyPort = new TextBox();
-            checkBoxRenewFilteredProxiesExclude2 = new CheckBox();
-            textBoxValue2 = new TextBox();
-            textBoxRenewFilteredProxiesFilterType = new TextBox();
+            checkBoxPort = new CheckBox();
+            textBoxValuePort = new TextBox();
+            buttonFilterPort = new Button();
+            checkBoxHttps = new CheckBox();
+            buttonFilterHttps = new Button();
+            checkBoxAnonymity = new CheckBox();
+            textBoxAnonyminty = new TextBox();
+            buttonFilterAnonymity = new Button();
+            checkBoxHttpsIsSupported = new CheckBox();
             SuspendLayout();
             // 
             // buttonGetProxies
@@ -103,7 +109,7 @@
             // 
             // buttonGetIndex
             // 
-            buttonGetIndex.Location = new Point(12, 484);
+            buttonGetIndex.Location = new Point(520, 47);
             buttonGetIndex.Name = "buttonGetIndex";
             buttonGetIndex.Size = new Size(148, 30);
             buttonGetIndex.TabIndex = 5;
@@ -123,7 +129,7 @@
             // 
             // buttonAddProxy
             // 
-            buttonAddProxy.Location = new Point(12, 520);
+            buttonAddProxy.Location = new Point(520, 93);
             buttonAddProxy.Name = "buttonAddProxy";
             buttonAddProxy.Size = new Size(148, 30);
             buttonAddProxy.TabIndex = 7;
@@ -131,19 +137,19 @@
             buttonAddProxy.UseVisualStyleBackColor = true;
             buttonAddProxy.Click += ButtonAddProxy_Click;
             // 
-            // buttonRenewFilteredProxies
+            // buttonFilterCountry
             // 
-            buttonRenewFilteredProxies.Location = new Point(12, 443);
-            buttonRenewFilteredProxies.Name = "buttonRenewFilteredProxies";
-            buttonRenewFilteredProxies.Size = new Size(148, 30);
-            buttonRenewFilteredProxies.TabIndex = 8;
-            buttonRenewFilteredProxies.Text = "RenewFilteredProxies";
-            buttonRenewFilteredProxies.UseVisualStyleBackColor = true;
-            buttonRenewFilteredProxies.Click += ButtonRenewFilteredProxies_Click;
+            buttonFilterCountry.Location = new Point(520, 146);
+            buttonFilterCountry.Name = "buttonFilterCountry";
+            buttonFilterCountry.Size = new Size(148, 30);
+            buttonFilterCountry.TabIndex = 8;
+            buttonFilterCountry.Text = "FilterByCountry";
+            buttonFilterCountry.UseVisualStyleBackColor = true;
+            buttonFilterCountry.Click += buttonFilterCountry_Click;
             // 
             // richTextBoxProxies
             // 
-            richTextBoxProxies.Location = new Point(166, 28);
+            richTextBoxProxies.Location = new Point(178, 28);
             richTextBoxProxies.Name = "richTextBoxProxies";
             richTextBoxProxies.ReadOnly = true;
             richTextBoxProxies.Size = new Size(321, 381);
@@ -160,18 +166,18 @@
             buttonClearLog.UseVisualStyleBackColor = true;
             buttonClearLog.Click += ButtonClearLog_Click;
             // 
-            // textBoxValue1
+            // textBoxCountry
             // 
-            textBoxValue1.Location = new Point(237, 448);
-            textBoxValue1.Name = "textBoxValue1";
-            textBoxValue1.PlaceholderText = "Value1";
-            textBoxValue1.Size = new Size(81, 23);
-            textBoxValue1.TabIndex = 11;
+            textBoxCountry.Location = new Point(674, 150);
+            textBoxCountry.Name = "textBoxCountry";
+            textBoxCountry.PlaceholderText = "Value";
+            textBoxCountry.Size = new Size(130, 23);
+            textBoxCountry.TabIndex = 11;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(166, 9);
+            label3.Location = new Point(180, 9);
             label3.Name = "label3";
             label3.Size = new Size(33, 15);
             label3.TabIndex = 15;
@@ -179,92 +185,158 @@
             // 
             // textBoxAddProxyIP
             // 
-            textBoxAddProxyIP.Location = new Point(166, 525);
+            textBoxAddProxyIP.Location = new Point(674, 98);
             textBoxAddProxyIP.Name = "textBoxAddProxyIP";
             textBoxAddProxyIP.PlaceholderText = "IP (e.g : 10.20.30.40)";
-            textBoxAddProxyIP.Size = new Size(152, 23);
+            textBoxAddProxyIP.Size = new Size(130, 23);
             textBoxAddProxyIP.TabIndex = 17;
             // 
             // textBoxGetIndex
             // 
-            textBoxGetIndex.Location = new Point(166, 489);
+            textBoxGetIndex.Location = new Point(674, 52);
             textBoxGetIndex.Name = "textBoxGetIndex";
             textBoxGetIndex.PlaceholderText = "Proxy in queue (e.g : 10.20.30.40:80)";
-            textBoxGetIndex.Size = new Size(321, 23);
+            textBoxGetIndex.Size = new Size(234, 23);
             textBoxGetIndex.TabIndex = 18;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(12, 412);
+            label4.Location = new Point(11, 412);
             label4.Name = "label4";
-            label4.Size = new Size(477, 15);
+            label4.Size = new Size(897, 15);
             label4.TabIndex = 19;
-            label4.Text = "______________________________________________________________________________________________";
+            label4.Text = "__________________________________________________________________________________________________________________________________________________________________________________";
             // 
-            // checkBoxRenewFilteredProxiesExclude1
+            // checkBoxCountry
             // 
-            checkBoxRenewFilteredProxiesExclude1.AutoSize = true;
-            checkBoxRenewFilteredProxiesExclude1.Location = new Point(404, 437);
-            checkBoxRenewFilteredProxiesExclude1.Name = "checkBoxRenewFilteredProxiesExclude1";
-            checkBoxRenewFilteredProxiesExclude1.Size = new Size(87, 19);
-            checkBoxRenewFilteredProxiesExclude1.TabIndex = 20;
-            checkBoxRenewFilteredProxiesExclude1.Text = "1. Exclude ?";
-            checkBoxRenewFilteredProxiesExclude1.UseVisualStyleBackColor = true;
+            checkBoxCountry.AutoSize = true;
+            checkBoxCountry.Location = new Point(810, 151);
+            checkBoxCountry.Name = "checkBoxCountry";
+            checkBoxCountry.Size = new Size(75, 19);
+            checkBoxCountry.TabIndex = 20;
+            checkBoxCountry.Text = "Exclude ?";
+            checkBoxCountry.UseVisualStyleBackColor = true;
             // 
             // textBoxAddProxyPort
             // 
-            textBoxAddProxyPort.Location = new Point(340, 525);
+            textBoxAddProxyPort.Location = new Point(810, 98);
             textBoxAddProxyPort.Name = "textBoxAddProxyPort";
             textBoxAddProxyPort.PlaceholderText = "Port (e.g : 80)";
-            textBoxAddProxyPort.Size = new Size(147, 23);
+            textBoxAddProxyPort.Size = new Size(98, 23);
             textBoxAddProxyPort.TabIndex = 21;
             // 
-            // checkBoxRenewFilteredProxiesExclude2
+            // checkBoxPort
             // 
-            checkBoxRenewFilteredProxiesExclude2.AutoSize = true;
-            checkBoxRenewFilteredProxiesExclude2.Location = new Point(404, 464);
-            checkBoxRenewFilteredProxiesExclude2.Name = "checkBoxRenewFilteredProxiesExclude2";
-            checkBoxRenewFilteredProxiesExclude2.Size = new Size(87, 19);
-            checkBoxRenewFilteredProxiesExclude2.TabIndex = 22;
-            checkBoxRenewFilteredProxiesExclude2.Text = "2. Exclude ?";
-            checkBoxRenewFilteredProxiesExclude2.UseVisualStyleBackColor = true;
+            checkBoxPort.AutoSize = true;
+            checkBoxPort.Location = new Point(810, 223);
+            checkBoxPort.Name = "checkBoxPort";
+            checkBoxPort.Size = new Size(75, 19);
+            checkBoxPort.TabIndex = 24;
+            checkBoxPort.Text = "Exclude ?";
+            checkBoxPort.UseVisualStyleBackColor = true;
             // 
-            // textBoxValue2
+            // textBoxValuePort
             // 
-            textBoxValue2.Location = new Point(324, 448);
-            textBoxValue2.Name = "textBoxValue2";
-            textBoxValue2.PlaceholderText = "Value2";
-            textBoxValue2.Size = new Size(69, 23);
-            textBoxValue2.TabIndex = 23;
+            textBoxValuePort.Location = new Point(674, 219);
+            textBoxValuePort.Name = "textBoxValuePort";
+            textBoxValuePort.PlaceholderText = "Value";
+            textBoxValuePort.Size = new Size(130, 23);
+            textBoxValuePort.TabIndex = 23;
             // 
-            // textBoxRenewFilteredProxiesFilterType
+            // buttonFilterPort
             // 
-            textBoxRenewFilteredProxiesFilterType.Location = new Point(166, 448);
-            textBoxRenewFilteredProxiesFilterType.Name = "textBoxRenewFilteredProxiesFilterType";
-            textBoxRenewFilteredProxiesFilterType.PlaceholderText = "Filter Type";
-            textBoxRenewFilteredProxiesFilterType.Size = new Size(65, 23);
-            textBoxRenewFilteredProxiesFilterType.TabIndex = 24;
+            buttonFilterPort.Location = new Point(520, 214);
+            buttonFilterPort.Name = "buttonFilterPort";
+            buttonFilterPort.Size = new Size(148, 30);
+            buttonFilterPort.TabIndex = 22;
+            buttonFilterPort.Text = "FilterByPort";
+            buttonFilterPort.UseVisualStyleBackColor = true;
+            buttonFilterPort.Click += buttonFilterPort_Click;
+            // 
+            // checkBoxHttps
+            // 
+            checkBoxHttps.AutoSize = true;
+            checkBoxHttps.Location = new Point(810, 371);
+            checkBoxHttps.Name = "checkBoxHttps";
+            checkBoxHttps.Size = new Size(75, 19);
+            checkBoxHttps.TabIndex = 30;
+            checkBoxHttps.Text = "Exclude ?";
+            checkBoxHttps.UseVisualStyleBackColor = true;
+            // 
+            // buttonFilterHttps
+            // 
+            buttonFilterHttps.Location = new Point(520, 363);
+            buttonFilterHttps.Name = "buttonFilterHttps";
+            buttonFilterHttps.Size = new Size(148, 30);
+            buttonFilterHttps.TabIndex = 28;
+            buttonFilterHttps.Text = "FilerByHttps";
+            buttonFilterHttps.UseVisualStyleBackColor = true;
+            buttonFilterHttps.Click += buttonFilterHttps_Click;
+            // 
+            // checkBoxAnonymity
+            // 
+            checkBoxAnonymity.AutoSize = true;
+            checkBoxAnonymity.Location = new Point(810, 294);
+            checkBoxAnonymity.Name = "checkBoxAnonymity";
+            checkBoxAnonymity.Size = new Size(75, 19);
+            checkBoxAnonymity.TabIndex = 27;
+            checkBoxAnonymity.Text = "Exclude ?";
+            checkBoxAnonymity.UseVisualStyleBackColor = true;
+            // 
+            // textBoxAnonyminty
+            // 
+            textBoxAnonyminty.Location = new Point(674, 292);
+            textBoxAnonyminty.Name = "textBoxAnonyminty";
+            textBoxAnonyminty.PlaceholderText = "Value";
+            textBoxAnonyminty.Size = new Size(130, 23);
+            textBoxAnonyminty.TabIndex = 26;
+            // 
+            // buttonFilterAnonymity
+            // 
+            buttonFilterAnonymity.Location = new Point(520, 288);
+            buttonFilterAnonymity.Name = "buttonFilterAnonymity";
+            buttonFilterAnonymity.Size = new Size(148, 30);
+            buttonFilterAnonymity.TabIndex = 25;
+            buttonFilterAnonymity.Text = "FilterByAnonymity";
+            buttonFilterAnonymity.UseVisualStyleBackColor = true;
+            buttonFilterAnonymity.Click += buttonFilterAnonymity_Click;
+            // 
+            // checkBoxHttpsIsSupported
+            // 
+            checkBoxHttpsIsSupported.AutoSize = true;
+            checkBoxHttpsIsSupported.Location = new Point(692, 370);
+            checkBoxHttpsIsSupported.Name = "checkBoxHttpsIsSupported";
+            checkBoxHttpsIsSupported.Size = new Size(99, 19);
+            checkBoxHttpsIsSupported.TabIndex = 31;
+            checkBoxHttpsIsSupported.Text = "Is supported ?";
+            checkBoxHttpsIsSupported.UseVisualStyleBackColor = true;
             // 
             // FormMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoSizeMode = AutoSizeMode.GrowAndShrink;
-            ClientSize = new Size(499, 562);
-            Controls.Add(textBoxRenewFilteredProxiesFilterType);
-            Controls.Add(textBoxValue2);
-            Controls.Add(checkBoxRenewFilteredProxiesExclude2);
+            ClientSize = new Size(920, 452);
+            Controls.Add(checkBoxHttpsIsSupported);
+            Controls.Add(checkBoxHttps);
+            Controls.Add(buttonFilterHttps);
+            Controls.Add(checkBoxAnonymity);
+            Controls.Add(textBoxAnonyminty);
+            Controls.Add(buttonFilterAnonymity);
+            Controls.Add(checkBoxPort);
+            Controls.Add(textBoxValuePort);
+            Controls.Add(buttonFilterPort);
             Controls.Add(textBoxAddProxyPort);
-            Controls.Add(checkBoxRenewFilteredProxiesExclude1);
+            Controls.Add(checkBoxCountry);
             Controls.Add(label4);
             Controls.Add(textBoxGetIndex);
             Controls.Add(textBoxAddProxyIP);
             Controls.Add(label3);
-            Controls.Add(textBoxValue1);
+            Controls.Add(textBoxCountry);
             Controls.Add(buttonClearLog);
             Controls.Add(richTextBoxProxies);
-            Controls.Add(buttonRenewFilteredProxies);
+            Controls.Add(buttonFilterCountry);
             Controls.Add(buttonAddProxy);
             Controls.Add(buttonPopProxy);
             Controls.Add(buttonGetIndex);
@@ -291,18 +363,24 @@
         private Button buttonGetIndex;
         private Button buttonPopProxy;
         private Button buttonAddProxy;
-        private Button buttonRenewFilteredProxies;
+        private Button buttonFilterCountry;
         private RichTextBox richTextBoxProxies;
         private Button buttonClearLog;
-        private TextBox textBoxValue1;
+        private TextBox textBoxCountry;
         private Label label3;
         private TextBox textBoxAddProxyIP;
         private TextBox textBoxGetIndex;
         private Label label4;
-        private CheckBox checkBoxRenewFilteredProxiesExclude1;
+        private CheckBox checkBoxCountry;
         private TextBox textBoxAddProxyPort;
-        private CheckBox checkBoxRenewFilteredProxiesExclude2;
-        private TextBox textBoxValue2;
-        private TextBox textBoxRenewFilteredProxiesFilterType;
+        private CheckBox checkBoxPort;
+        private TextBox textBoxValuePort;
+        private Button buttonFilterPort;
+        private CheckBox checkBoxHttps;
+        private Button buttonFilterHttps;
+        private CheckBox checkBoxAnonymity;
+        private TextBox textBoxAnonyminty;
+        private Button buttonFilterAnonymity;
+        private CheckBox checkBoxHttpsIsSupported;
     }
 }
--- ProxySharpTester/FormMain.cs
+++ ProxySharpTester/FormMain.cs
@@ -40,13 +40,6 @@ namespace ProxySharpTester
             Proxy.RenewQueue();
         }
 
-        private void ButtonRenewFilteredProxies_Click(object sender, EventArgs e)
-        {
-            Proxy.RenewFilteredProxies(Convert.ToInt32(textBoxRenewFilteredProxiesFilterType.Text), textBoxValue1.Text.ToUpper(), checkBoxRenewFilteredProxiesExclude1.Checked, textBoxValue2.Text.ToUpper(), checkBoxRenewFilteredProxiesExclude2.Checked);
-            textBoxValue1.Clear();
-            checkBoxRenewFilteredProxiesExclude1.Checked = false;
-        }
-
         private void ButtonGetIndex_Click(object sender, EventArgs e)
         {
             MessageBox.Show($"The index of {textBoxGetIndex.Text} is : {Proxy.GetIndex(textBoxGetIndex.Text)}");
@@ -67,6 +60,28 @@ namespace ProxySharpTester
         private void ButtonClearLog_Click(object sender, EventArgs e)
         {
             richTextBoxProxies.Clear();
+        }
+
+        private void buttonFilterCountry_Click(object sender, EventArgs e)
+        {
+            Proxy.FilterQueueByCountry(textBoxCountry.Text, checkBoxCountry.Checked);
+
+        }
+
+        private void buttonFilterPort_Click(object sender, EventArgs e)
+        {
+            Proxy.FilterQueueByPort(textBoxValuePort.Text, checkBoxPort.Checked);
+        }
+
+        private void buttonFilterAnonymity_Click(object sender, EventArgs e)
+        {
+            Proxy.FilterQueueByAnonymityLevel(Convert.ToInt32(textBoxAnonyminty.Text), checkBoxAnonymity.Checked);
+        }
+
+        private void buttonFilterHttps_Click(object sender, EventArgs e)
+        {
+            Proxy.FilterQueueByHttps(checkBoxHttpsIsSupported.Checked, checkBoxHttps.Checked);
+            ButtonGetProxies_Click(sender, e);
         }
     }
 }
